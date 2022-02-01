1c7c3b00251feeba93bf3798a56f1c014c072b69
Sathvik Parasa <sathvikparasa007@gmail.com>
2022-01-31 16:09:53 -0800

    Add motors and update controller
    Co-Authored-By: Michael Phillips <63265759+MichaelDaUnicorn@users.noreply.github.com>

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,11 +5,13 @@ extern brain Brain;
 // VEXcode devices
 extern drivetrain Drivetrain;
 extern controller Controller1;
-extern motor Intake;
+extern motor ScissorLift;
+extern motor Hook;
+extern motor Conveyor;
 
 /**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *  
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
  * This should be called at the start of your int main function.
  */
 void  vexcodeInit( void );
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,19 +10,24 @@ brain  Brain;
 // VEXcode device constructors
 motor LeftDriveSmart = motor(PORT1, ratio18_1, false);
 motor RightDriveSmart = motor(PORT10, ratio18_1, true);
-drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 406.4, 406.4, mm, 1.4);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 355.59999999999997, 330.2, mm, 0.625);
 controller Controller1 = controller(primary);
-motor Intake = motor(PORT15, ratio6_1, false);
+motor ScissorLift = motor(PORT15, ratio18_1, false);
+motor Hook = motor(PORT17, ratio18_1, false);
+motor Conveyor = motor(PORT19, ratio18_1, false);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
 // define variables used for controlling motors based on controller inputs
+bool Controller1LeftShoulderControlMotorsStopped = true;
+bool Controller1RightShoulderControlMotorsStopped = true;
+bool Controller1XBButtonsControlMotorsStopped = true;
 bool DrivetrainLNeedsToBeStopped_Controller1 = true;
 bool DrivetrainRNeedsToBeStopped_Controller1 = true;
 
 // define a task that will handle monitoring inputs from Controller1
-int rc_auto_loop_function_Controller1() {
+int rc_auto_loop_callback_Controller1() {
   // process the controller input every 20 milliseconds
   // update the motors based on the input values
   while(true) {
@@ -32,7 +37,6 @@ int rc_auto_loop_function_Controller1() {
       // right = Axis3 - Axis1
       int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
       int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
-      
       // check if the value is inside of the deadband range
       if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
         // check if the left motor has already been stopped
@@ -43,7 +47,7 @@ int rc_auto_loop_function_Controller1() {
           DrivetrainLNeedsToBeStopped_Controller1 = false;
         }
       } else {
-        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        // reset the toggle so that the deadband code knows to stop the left motor next time the input is in the deadband range
         DrivetrainLNeedsToBeStopped_Controller1 = true;
       }
       // check if the value is inside of the deadband range
@@ -59,7 +63,6 @@ int rc_auto_loop_function_Controller1() {
         // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
         DrivetrainRNeedsToBeStopped_Controller1 = true;
       }
-      
       // only tell the left drive motor to spin if the values are not in the deadband range
       if (DrivetrainLNeedsToBeStopped_Controller1) {
         LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
@@ -70,6 +73,41 @@ int rc_auto_loop_function_Controller1() {
         RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
         RightDriveSmart.spin(forward);
       }
+      // check the ButtonL1/ButtonL2 status to control Hook
+      if (Controller1.ButtonL1.pressing()) {
+        Hook.spin(forward);
+        Controller1LeftShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonL2.pressing()) {
+        Hook.spin(reverse);
+        Controller1LeftShoulderControlMotorsStopped = false;
+      } else if (!Controller1LeftShoulderControlMotorsStopped) {
+        Hook.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1LeftShoulderControlMotorsStopped = true;
+      }
+      // check the ButtonR1/ButtonR2 status to control ScissorLift
+      if (Controller1.ButtonR1.pressing()) {
+        ScissorLift.spin(forward);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonR2.pressing()) {
+        ScissorLift.spin(reverse);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (!Controller1RightShoulderControlMotorsStopped) {
+        ScissorLift.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1RightShoulderControlMotorsStopped = true;
+      }
+      // check the X/B buttons status to control Conveyor
+      if (Controller1.ButtonX.pressing()) {
+        Conveyor.spin(forward);
+        Controller1XBButtonsControlMotorsStopped = false;
+      } else if (Controller1.ButtonB.pressing()) {
+        Conveyor.spin(reverse);
+        Controller1XBButtonsControlMotorsStopped = false;
+      } else if (!Controller1XBButtonsControlMotorsStopped){
+        Conveyor.stop();
+        Controller1XBButtonsControlMotorsStopped = true;
+      }
     }
     // wait before repeating the process
     wait(20, msec);
@@ -78,10 +116,10 @@ int rc_auto_loop_function_Controller1() {
 }
 
 /**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
  * 
  * This should be called at the start of your int main function.
  */
 void vexcodeInit( void ) {
-  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+  task rc_auto_loop_task_Controller1(rc_auto_loop_callback_Controller1);
 }
