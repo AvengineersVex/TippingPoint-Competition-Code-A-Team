d2e9ca96bbf5c0a197fbef97cdd70b2d985575e8
Michael Phillips <63265759+MichaelDaUnicorn@users.noreply.github.com>
2022-02-03 12:38:54 -0800

    Add start and stop "recording"

--- src/rerun.cpp
+++ src/rerun.cpp
@@ -17,8 +17,15 @@ void rerun() {
   autonomousProgram << "#include \"vex.h\"" << std::endl << std::endl;
   autonomousProgram << "using namespace vex;" << std::endl << std::endl;
   
-  // while(true) {
-  for(int i = 0; i < 10; i ++) {
+  while(true) {
+    task::sleep(10);
+    if(Controller.ButtonUp.pressing()) {
+      break;
+    }
+  }
+  
+  while(true) {
+//   for(int i = 0; i < 10; i ++) {
 
     // autonomousProgram << "motor1.setVelocity(" << motor1.velocity() << ", pct" << std::endl;
     // autonomousProgram << "motor2.setVelocity(" << motor2.velocity() << ", pct" << std::endl;
@@ -37,11 +44,11 @@ void rerun() {
     previousTime = 10;
 
     autonomousProgram << "task::sleep(" << timeDifference << ");" << std::endl << std::endl;
+    
+    if(Controller.ButtonDown.pressing()) {
+      break;
+    }
   }
 
   autonomousProgram.close();
-}
-
-int main() {
-  rerun();
 }
